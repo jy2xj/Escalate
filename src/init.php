2631385456fde255dbe4fafe2c1040ca3fbe7db3
Joshua <jy2xj@virginia.edu>
2016-11-19 19:03:15 -0500

    add init database

--- src/init.php
+++ src/init.php
@@ -16,11 +16,44 @@
 			die ("Could not connect to db: " . $db->connect_error);
 		endif;
 
-		$db->query("drop table Users"); //Drop the previously created tables
+		$db->query("drop table Users, Students, Quizzes"); //Drop the previously created tables
 		
 		//Create the tables with the correct fields
-		$result = $db->query("create table Users (user_id varchar(30) primary key not null, password varchar(255), first_name char(40) not null, last_name char(40))") or die ("Invalid: " . $db->error);
+		$result = $db->query("create table Users (user_id varchar(30) primary key not null, password varchar(255), first_name char(40) not null, last_name char(40), auth char(40))") or die ("Invalid: " . $db->error);
+		$result = $db->query("create table Students (user_id varchar(30) primary key not null, tutor tinyint)") or die ("Invalid: " . $db->error);
+		$result = $db->query("create table Quizzes (user_id varchar(30) primary key not null, Quiz_1 int, Quiz_2 int, Quiz_3 int, Chapter3_Test int)") or die ("Invalid: " . $db->error);
+
+
+		$fileptr = fopen("users.txt", "rw");
+		if ($fileptr) {
+    		while (($line = fgets($fileptr)) !== false) {
+       			$pieces = explode("#", $line);
+        		$password = hash("sha256", "$pieces[1]");
+    			$sql = $db->query("INSERT INTO Users VALUES ('$pieces[0]','$password','$pieces[2]','$pieces[3]','$pieces[4]')") or die ("Invalid: " . $db->error);
+    		}
+    	fclose($fileptr);
+		}
+
+		$fileptr = fopen("quizzes.txt", "rw");
+		if ($fileptr) {
+    		while (($line = fgets($fileptr)) !== false) {
+       			$pieces = explode("#", $line);
+    			$sql = $db->query("INSERT INTO Quizzes VALUES ('$pieces[0]','$pieces[1]','$pieces[2]','$pieces[3]','$pieces[4]')") or die ("Invalid: " . $db->error);
+    		}
+    	fclose($fileptr);
+		}
+
+		$fileptr = fopen("students.txt", "rw");
+		if ($fileptr) {
+    		while (($line = fgets($fileptr)) !== false) {
+       			$pieces = explode("#", $line);
+    			$sql = $db->query("INSERT INTO Students VALUES ('$pieces[0]','$pieces[1]')") or die ("Invalid: " . $db->error);
+    		}
+    	fclose($fileptr);
+		}
+
+		header("Location: login.php");
+		exit;
 		?>
-		Hello World!
 	</body>
 </html>
