c4bc6ab2d8c107b49d219335a62a71e665e7d222
bigjohnlin <bigjohnlin@gmail.com>
2016-11-20 10:48:28 -0500

    progress bar alignment fix

--- src/student.php
+++ src/student.php
@@ -110,58 +110,55 @@ while($row = mysqli_fetch_assoc($result)) {
   echo    "<tr>";
   echo         '<td>Quiz 1</td>';
   echo         '<td>';
-  echo                    $score1;
+  echo                    "$score1%";
   echo         "</td>";
   checkScore($score1);
   echo        '<td><button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo1"> Show Details </button></td>';
   echo     "</tr>";
   echo      '<tr><td id="space" colspan="4" padding="10px"><div id="demo1" class = "collapse">';
   echo '<table id=barGraph><tr><td id="space" colspan="4"><div class="progress-bar progress-bar-success" ';
-  echo 'role="progressbar" aria-valuenow=$score aria-valuemin="0" aria-valuemax="100" style="width: '. $score1.'% ">';
+  echo 'role="progressbar" aria-valuemin="0" aria-valuemax="100" float="left" style="width: '. $score1.'% ">';
   echo  "Your Score: $score1%";
   echo ' </div></td></tr>
-  <tr><td></td></tr>
-    <tr><td id="space" colspan="4"><div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: '. $avg.'% ">';
+    <tr><td id="space" colspan="4"><div class="progress-bar progress-bar-danger" role="progressbar"  aria-valuemin="0" aria-valuemax="100" style="width: '. $avg.'% ">';
   echo    "Average: $avg%";
   echo  '</div></td></tr></table>
 </div></td></tr>';
 
-  echo    "<tr>";
-  echo         '<td>Quiz 2</td>';
-  echo         '<td>';
-  echo                    $score2;
-  echo         "</td>";
-  checkScore($score2);
-  echo        '<td><button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo2"> Show Details </button></td>';
-  echo     "</tr>";
-  echo "<br>";
-  echo      '<tr><td id="space" colspan="4"><div id="demo2" class = "collapse">';
-  echo '<table id=barGraph><tr><td colspan="4"><div class="progress-bar progress-bar-success" ';
-  echo 'role="progressbar" aria-valuenow=$score aria-valuemin="0" aria-valuemax="100" style="width: '. $score2.'% ">';
-  echo  "Your Score: $score2%";
-  echo ' </div></td></tr>
-    <tr><td></td></tr>
-    <tr><td id="space" colspan="4"><div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:'. $avg1.'% ">';
-  echo    "Average: $avg1%";
-  echo  '</div></td></tr></table>
-</div></td></tr>';
+echo    "<tr>";
+echo         '<td>Quiz 2</td>';
+echo         '<td>';
+echo                    "$score2%";
+echo         "</td>";
+checkScore($score2);
+echo        '<td><button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo2"> Show Details </button></td>';
+echo     "</tr>";
+echo      '<tr><td id="space" colspan="4" padding="10px"><div id="demo2" class = "collapse">';
+echo '<table id=barGraph><tr><td id="space" colspan="4"><div class="progress-bar progress-bar-success" ';
+echo 'role="progressbar" aria-valuemin="0" aria-valuemax="100" float="left" style="width: '. $score2.'% ">';
 
-  echo         '<td>Quiz 3</td>';
-  echo         '<td>';
-  echo                    $score3;
-  echo         "</td>";
-  checkScore($score3);
-  echo        '<td><button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo3"> Show Details </button></td>';
-  echo     "</tr>";
-  echo      '<tr><td id="space" colspan="4"><div id="demo3" class = "collapse">';
-  echo '<table id=barGraph><tr><td colspan="4"><div class="progress-bar progress-bar-success" ';
-  echo 'role="progressbar" aria-valuenow=$score aria-valuemin="0" aria-valuemax="100" style="width: '. $score3.'% ">';
-  echo  "Your Score: $score3%";
-  echo ' </div></td></tr>
-    <tr><td></td></tr>
-    <tr><td id="space" colspan="4"><div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:'. $avg2.'% ">';
-  echo    "Average: $avg2%";
-  echo  '</div></td></tr></table>
+echo  "Your Score: $score3%";
+echo ' </div></td></tr>
+  <tr><td id="space" colspan="4"><div class="progress-bar progress-bar-danger" role="progressbar"  aria-valuemin="0" aria-valuemax="100" style="width: '. $avg2.'% ">';
+echo    "Average: $avg2%";
+echo  '</div></td></tr></table></div></td></tr>';
+
+echo    "<tr>";
+echo         '<td>Quiz 3</td>';
+echo         '<td>';
+echo                    "$score3%";
+echo         "</td>";
+checkScore($score3);
+echo        '<td><button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo3"> Show Details </button></td>';
+echo     "</tr>";
+echo      '<tr><td id="space" colspan="4" padding="10px"><div id="demo3" class = "collapse">';
+echo '<table id=barGraph><tr><td id="space" colspan="4"><div class="progress-bar progress-bar-success" ';
+echo 'role="progressbar" aria-valuemin="0" aria-valuemax="100" float="left" style="width: '. $score3.'% ">';
+echo  "Your Score: $score3%";
+echo ' </div></td></tr>
+  <tr><td id="space" colspan="4"><div class="progress-bar progress-bar-danger" role="progressbar"  aria-valuemin="0" aria-valuemax="100" style="width: '. $avg2.'% ">';
+echo    "Average: $avg2%";
+echo  '</div></td></tr></table>
 </div></td></tr>';
   echo "</table>";
   echo "<br>";
