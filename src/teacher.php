54e83089a4f02fb73f4d88c64a6d1193d7e0413a
Joshua <jy2xj@virginia.edu>
2016-11-20 11:25:22 -0500

    update teacher.php

--- src/teacher.php
+++ src/teacher.php
@@ -20,7 +20,6 @@
 
 </head>
 <body>
-<div id='students_info'>
 <?php
     $conn = new mysqli('localhost', 'root', '', 'phpmyadmin');
     if ($conn->connect_error):
@@ -36,33 +35,92 @@
       echo "<p id = 'test'>";
       echo  "Teacher: $teacherID - $firstName $lastName</p>";
     }
-
-    /*$sql = "SELECT Quiz_1, Quiz_2, Quiz_3
-          FROM quizzes
-          WHERE user_id = '$studentID' ";
-	//$sql = "SELECT * FROM quizzes";
-	$result = $db->query($sql);
-
-	if(!$result) {
-	  echo "Could not run Query from Database" . mysql_error();
-	  exit;
-	}*/
+    echo '<div id="students_info">';
     ?>
     <div id="myDiv" style="width: 480px; height: 400px;"><!-- Plotly chart will be drawn inside this DIV --></div>
      <script type="text/javascript" language="javascript">
-  	function getData() {
-  		var data = [{
-		  type: 'bar',
-		  x: [20, 14, 23],
-		  y: ['giraffes', 'orangutans', 'monkeys'],
-		  orientation: 'h'
-		}];
+     	var info_array;
+	  	function getData(info) {
+	  		var arrayLength = info.length+1;
+	  		var student_names = [];
+	  		var quizzes_taken = [];
+	  		var quizzes_needed = [];
+	  		var total = 0;
+	  		var difference;
+			for (var i = 0; i < arrayLength-1; i++) {
+	    		//alert(info[i]['current_prog']);
+	    		var name_full = info[i]['first_name'] + " " + info[i]['last_name'];
+	    		student_names[i] = name_full;
+	    		quizzes_taken[i] = info[i]['current_prog'];
+	    		total += parseInt(quizzes_taken[i], 10);
+	    		difference = info[i]['total_prog'] - info[i]['current_prog'];
+	    		quizzes_needed[i] = difference;
+	    	}
+	    	student_names[arrayLength-1] = "Class Average";
+    		quizzes_taken[arrayLength-1] = String(total/(arrayLength-1));
+    		difference = info[0]['total_prog'] - quizzes_taken[arrayLength-1];
+    		quizzes_needed[arrayLength-1] = difference;
+	  		var trace1 = {
+			  x: quizzes_taken,
+			  y: student_names,
+			  name: 'Quizzes Taken',
+			  orientation: 'h',
+			  marker: {
+			    color: 'rgba(55,128,191,0.6)',
+			    width: 1
+			  },
+			  type: 'bar'
+			};
 
-		Plotly.newPlot('myDiv', data);
-  	}
-  	getData();
+			var trace2 = {
+			  x: quizzes_needed,
+			  y: student_names,
+			  name: 'Quizzes Left',
+			  orientation: 'h',
+			  type: 'bar',
+			  marker: {
+			    color: 'rgba(255,153,51,0.6)',
+			    width: 1
+			  }
+			};
+
+			var data = [trace1, trace2];
+
+			var layout = {
+			  title: 'Results for Your Class',
+			  barmode: 'stack'
+			};
+
+			Plotly.newPlot('myDiv', data, layout);
+	  	}
   </script>
-</div>
+   </div>
+  <?php
+
+  $sql = "SELECT *
+          FROM Quizzes, Students, Users
+          WHERE Students.teacher_id = '$teacherID' AND Quizzes.user_id=Students.user_id AND Students.user_id=Users.user_id";
+	//$sql = "SELECT * FROM quizzes";
+	$result = $conn->query($sql);
+	if(!$result) {
+	  echo "Could not run Query from Database" . mysql_error();
+	  exit;
+	}
+	while($row = $result->fetch_assoc()){
+		$data[] = $row;
+	}
+
+	if (isset($data)) {
+		$info_array = json_encode($data);
+	}
+  echo '<script type="text/javascript">',
+     "getData($info_array);",
+     '</script>';
+    ?>
+  <form action="logout.php"
+  method = "POST">
+  <input type="submit"style="color:blue;width:100;height:49;font-size:20px;" value="Logout"style= "background-color: #ff5151;"></input>
+  </form>
 
 </body>
 </html>
